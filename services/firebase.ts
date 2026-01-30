8aa9edb28af0fb664d7867ca8e1dd989c73d64ff
sanjaisakthivel2006-cloud <sanjaisakthivel2006@gmail.com>
2026-01-30 11:50:55 +0530

    feat: Implement Firebase integration by adding the dependency and initialization file.

---
+++ services/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyALU-wFTyOU8c79NOawlwQT4WX1JEBsRsY",
+  authDomain: "college-student-profile.firebaseapp.com",
+  projectId: "college-student-profile",
+  storageBucket: "college-student-profile.firebasestorage.app",
+  messagingSenderId: "353023141578",
+  appId: "1:353023141578:web:f943a1265c3c512da5b633"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
